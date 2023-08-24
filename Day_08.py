2cb9bb37455096e97af82d8a8609e81a1a894e95
Ayush Thakur <ayushthakur1412@gmail.com>
2023-08-24 13:21:42 +0530

    updated

--- Day_08.py
+++ Day_08.py
@@ -1,29 +1,31 @@
-def fill_water_tanks(buildings, r, unit_capacity):
-    building_list = [(i, water) for i, water in enumerate(buildings)]
-    
-    building_list.sort(key=lambda x: x[1], reverse=True)
-    
-    tanks_filled = 0
-    filled_buildings = []
-    
-    for building, water in building_list:
-        tanks_needed = (water + unit_capacity - 1) // unit_capacity 
-        tanks_to_fill = min(r - tanks_filled, tanks_needed) 
-        
-        if tanks_to_fill > 0:
-            tanks_filled += tanks_to_fill
-            filled_buildings.append(building)
-        
-        if tanks_filled >= r:
-            break
-    
-    return filled_buildings
 
-# Example usage
-n = 5
-water_availabilities = [10, 8, 15, 7, 12]
-r = 3
-unit_capacity = 5
 
-result = fill_water_tanks(water_availabilities, r, unit_capacity)
-print("Buildings to fill tanks:", result)
+# def fill_water_tanks(buildings, r, unit_capacity):
+#     building_list = [(i, water) for i, water in enumerate(buildings)]
+    
+#     building_list.sort(key=lambda x: x[1], reverse=True)
+    
+#     tanks_filled = 0
+#     filled_buildings = []
+    
+#     for building, water in building_list:
+#         tanks_needed = (water + unit_capacity - 1) // unit_capacity 
+#         tanks_to_fill = min(r - tanks_filled, tanks_needed) 
+        
+#         if tanks_to_fill > 0:
+#             tanks_filled += tanks_to_fill
+#             filled_buildings.append(building)
+        
+#         if tanks_filled >= r:
+#             break
+    
+#     return filled_buildings
+
+# # Example usage
+# n = 5
+# water_availabilities = [10, 8, 15, 7, 12]
+# r = 3
+# unit_capacity = 5
+
+# result = fill_water_tanks(water_availabilities, r, unit_capacity)
+# print("Buildings to fill tanks:", result)
